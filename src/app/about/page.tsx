895ebb80fee242606ec20166538912b43b3e95e1
israt Jahan Naishe <israt.ict.cou@gmail.com>
2025-03-16 13:08:20 +0200

    Created home page and about us page and Linked them

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About us</h1>
+      <p>
+        We are a team of passionate developers working to create innovative and
+        impactful solutions. Our mission is to provide quality services that
+        exceed expectations.
+      </p>
+    </div>
+  )
+}
+export default AboutPage
